0227fb5b5e6cfe4d1b39c960e949b4aabc11ebb1
Dannny-11 <daniel19h94@yahoo.com>
2025-02-13 14:04:05 -0600

    Algorithm
    Added Algorithm

--- NearMissFermat.java
+++ NearMissFermat.java
@@ -7,6 +7,7 @@ public class NearMissFermat {
         // Get n exponent
         int n = getExponent(scanner);
         int k = getUpperBound(scanner);
+        algorithm(n, k);
 
     }
 
@@ -51,6 +52,49 @@ public class NearMissFermat {
             }
         }
         return k;
+    }
+
+    public static void algorithm(int n, int k){
+        int newX = 0;
+        int newY = 0;
+        int newZ = 0;
+        int z = 0;
+        double smallestRM = 999999999;
+        for (int x = 10; x <= k; x++) {
+            for (int y = 10; y <= k; y++) {
+                int sum = (int) Math.pow(x, n) + (int) Math.pow(y, n);
+                z = (int) Math.pow(sum, 1.0 / n);
+                
+                int z1 = (int) Math.pow(z, n);
+                int z2 = (int) Math.pow(z + 1, n);                
+                
+                int miss1 = sum - z1;
+                int miss2 = z2 - sum;                
+                int miss = Math.min(miss1, miss2);
+                
+                //Print for debugging
+                //System.out.printf("z: %d z^n: %d\n", z,(int)Math.pow(z, n));
+                //System.out.printf("z+1: %d z^n: %d\n", (z+1),(int)Math.pow((z+1), n));
+
+                if(miss == miss2)
+                    z = z + 1;
+                
+                double relativeMiss = (double) miss / sum;
+                
+                //Print for debugging
+                System.out.printf("Current x: %d y: %d z: %d Sum: %d Miss: %d RM: %.5f%%\n", 
+                                x,y,z,sum,miss,relativeMiss);
+                
+                if (relativeMiss < smallestRM) { 
+                    newX = x;
+                    newY = y;
+                    newZ = z;  
+                    smallestRM = relativeMiss;                                   
+                    System.out.printf("Smallest results- x: %d y: %d z: %d Sum: %d Relative miss: %.10f%%\n\n", 
+                                      newX, newY, newZ, sum, smallestRM * 100);
+                }
+            }
+        }
     }
 
 }
