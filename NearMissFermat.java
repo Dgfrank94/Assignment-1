68142b4e12e335a4dcdbf2856fc723e74ae40c44
Dannny-11 <daniel19h94@yahoo.com>
2025-02-14 00:12:34 -0600

    Added comments

--- NearMissFermat.java
+++ NearMissFermat.java
@@ -1,3 +1,20 @@
+/* Title
+ * NearMissFermat.java
+ * 
+ * 
+ * Daniel Hernandez
+ * danielhernandez1@lewisu.edu
+ * SP25-CPSC-44000-LT1
+ * (Date finshed)
+ * (Explaantion of program)
+ * etc
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
 import java.util.Scanner;
 
 public class NearMissFermat {
@@ -54,44 +71,54 @@ public class NearMissFermat {
         return k;
     }
 
+    /* Runs the algorithm to find near misses on x^n+y^n=z^n.
+     * @param n Integer for the power
+     * @param k Integer for the upperbound limit
+     * 
+    */
     public static void algorithm(int n, int k){
-        int newX = 0;
-        int newY = 0;
-        int newZ = 0;
-        int z = 0;
-        double smallestRM = 999999999;
-        for (int x = 10; x <= k; x++) {
-            for (int y = 10; y <= k; y++) {
-                int sum = (int) Math.pow(x, n) + (int) Math.pow(y, n);
-                z = (int) Math.pow(sum, 1.0 / n);
-                
-                int z1 = (int) Math.pow(z, n);
-                int z2 = (int) Math.pow(z + 1, n);                
-                
-                int miss1 = sum - z1;
-                int miss2 = z2 - sum;                
-                int miss = Math.min(miss1, miss2);
-                
-                //Print for debugging
-                //System.out.printf("z: %d z^n: %d\n", z,(int)Math.pow(z, n));
-                //System.out.printf("z+1: %d z^n: %d\n", (z+1),(int)Math.pow((z+1), n));
+        int newX = 0; // Takes the x variable from the smallest miss
+        int newY = 0; // Takes the y variable from the smallest miss
+        int newZ = 0; // Takes the z variable from the smallest miss
+        int z = 0; // Initalizes z for the equation
+        double smallestRM = 999999999; // Variable that will contain the smallest Relative Miss
 
+        /* For loop that will intialize the beginning of the equation x^n+y^n=z^n. 
+         * X is set to 10 as a requiement of the assignment.
+         * Cycles through until x > k. 
+         */
+        for (int x = 10; x <= k; x++) {
+            // For loop that sets y = 10 and cycles until y > k
+            for (int y = 10; y <= k; y++) {
+                int sum = (int) Math.pow(x, n) + (int) Math.pow(y, n); // Adds x^n and y^n into the variable sum
+                z = (int) Math.pow(sum, 1.0 / n); // Finds the nth square root of the sum and sets it to z as a whole number
+                
+                int z1 = (int) Math.pow(z, n); // Finds the sum of z to the nth power and sets it to z1
+                int z2 = (int) Math.pow(z + 1, n); // Finds the sum of z+1 to the nth power and sets it to z2              
+                
+                int miss1 = sum - z1; // Subtracts z1 from the sum
+                int miss2 = z2 - sum; // Subtracts the sum from z2                
+                int miss = Math.min(miss1, miss2); // miss = the smaller variable
+
+                // Checks if miss is equal to miss2
                 if(miss == miss2)
                     z = z + 1;
                 
-                double relativeMiss = (double) miss / sum;
+                double relativeMiss = (double) miss / sum; // miss is divided by sum with the new sum being a double variable for precision
                 
                 //Print for debugging
-                System.out.printf("Current x: %d y: %d z: %d Sum: %d Miss: %d RM: %.5f%%\n", 
-                                x,y,z,sum,miss,relativeMiss);
+                // System.out.printf("Current x: %d y: %d z: %d Sum: %d Miss: %d RM: %.5f%%\n", 
+                //                 x,y,z,sum,miss,relativeMiss);
                 
+                // Checks if relativeMiss is less than smallestRM
                 if (relativeMiss < smallestRM) { 
                     newX = x;
                     newY = y;
                     newZ = z;  
-                    smallestRM = relativeMiss;                                   
-                    System.out.printf("Smallest results- x: %d y: %d z: %d Sum: %d Relative miss: %.10f%%\n\n", 
-                                      newX, newY, newZ, sum, smallestRM * 100);
+                    smallestRM = relativeMiss;
+                    
+                    System.out.printf("Current smallest results x: %d y: %d z: %d Miss: %d Relative miss: %.15f%%\n", 
+                                      newX, newY, newZ, miss, smallestRM * 100);
                 }
             }
         }
